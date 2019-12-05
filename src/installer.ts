70c110077d09237f456363dda435ec9dc7034000
Stanley Goldman <Stanley.Goldman@gmail.com>
2019-12-05 01:43:09 -0500

    Fixes

--- src/installer.ts
+++ src/installer.ts
@@ -1,13 +1,15 @@
 import * as path from "path";
+import * as process from "process";
 import { copySync } from "fs-extra";
 
 export function Install() {
   const loggerPath = path.join(
     path.dirname(__filename),
+    "..",
     "GitHubActionsMSBuildLogger.dll"
   );
   const targetLoggerPath = path.join(
-    __dirname,
+    process.cwd(),
     "..",
     "GitHubActionsMSBuildLogger.dll"
   );

92f1f61c6cf06c99a3da3928b062bc7e14a41df0
Stanley Goldman <Stanley.Goldman@gmail.com>
2019-12-05 01:11:19 -0500

    Fixing more things

--- src/installer.ts
+++ src/installer.ts
@@ -1,9 +1,16 @@
-import * as path from 'path';
-import {copySync} from 'fs-extra'
+import * as path from "path";
+import { copySync } from "fs-extra";
 
-export function Install(){
-  const loggerPath = path.join(path.dirname(__filename), "GitHubActionsMSBuildLogger.dll")
-  const targetLoggerPath = path.join(__dirname, '..', 'GitHubActionsMSBuildLogger.dll');
+export function Install() {
+  const loggerPath = path.join(
+    path.dirname(__filename),
+    "GitHubActionsMSBuildLogger.dll"
+  );
+  const targetLoggerPath = path.join(
+    __dirname,
+    "..",
+    "GitHubActionsMSBuildLogger.dll"
+  );
 
   copySync(loggerPath, targetLoggerPath);
 
--- src/setup-GitHubActionsMSBuildLogger.ts
+++ src/setup-GitHubActionsMSBuildLogger.ts
@@ -1,5 +1,5 @@
-import * as core from '@actions/core';
-import * as installer from './installer';
+import * as core from "@actions/core";
+import * as installer from "./installer";
 
 async function run() {
   try {

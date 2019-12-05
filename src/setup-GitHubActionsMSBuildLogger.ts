4f0208b861213aaddf978b0653a3d604bc0ee093
Stanley Goldman <Stanley.Goldman@gmail.com>
2019-12-05 01:34:57 -0500

    Necessary bits

--- src/setup-GitHubActionsMSBuildLogger.ts
+++ src/setup-GitHubActionsMSBuildLogger.ts
@@ -4,7 +4,7 @@ import * as installer from "./installer";
 async function run() {
   try {
     var path = installer.Install();
-    console.log(`::warning::Extracted GitHubActionsMSBuildLogger to ${path}`);
+    console.log(`Copied GitHubActionsMSBuildLogger to "${path}"`);
   } catch (error) {
     core.setFailed(error.message);
   }
